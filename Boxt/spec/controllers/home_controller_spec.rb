ae3df2a148eb08e65e10441349c7e26bf78a0011
Gargi Patil <gp2723@columbia.edu>
2023-11-01 21:48:02 +0000

    Rspec tests show, create, destroy

--- Boxt/spec/controllers/home_controller_spec.rb
+++ Boxt/spec/controllers/home_controller_spec.rb
@@ -2,23 +2,31 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
-describe 'create storage' do
-  it 'should create a new storage' do
-    expect(Storage.find_by(name:'Gargis home')).to be nil
-    put :create, storage: {name: 'Gargis home', available_space: 70, price: 10, campus_dist: 0.9, rating: 2.3, start_date: '10-May-2024', end_date: '10-Aug-2024' }
-    expect(Storage.find_by(name:'Gargis home')).to_not be nil
+describe 'show storage' do
+  it 'should show storage' do
+    s = Storage.create(name: 'Storage A', available_space: 70, price: 10, campus_dist: 0.9, rating: 2.3, start_date: '10-May-2024', end_date: '10-Aug-2024')
+    expect(Storage.find_by(name:'Storage A')).to_not be nil
+    put :show, id: s.id
+    expect(Storage.find_by(name:'Storage A')).to_not be nil
   end
 end
 
+describe 'create storage' do
+  it 'should create a new storage' do
+    expect(Storage.find_by(name:'Storage B')).to be nil
+    put :create, storage: {name: 'Storage B', available_space: 70, price: 10, campus_dist: 0.9, rating: 2.3, start_date: '10-May-2024', end_date: '10-Aug-2024' }
+    expect(Storage.find_by(name:'Storage B')).to_not be nil
+  end
+end
 
 describe 'delete storage' do
   it 'should delete an existing storage' do
 
-    s = Storage.create(name: 'Gargis home', available_space: 70, price: 10, campus_dist: 0.9, rating: 2.3, start_date: '10-May-2024', end_date: '10-Aug-2024')
-    expect(Storage.find_by(name:'Gargis home')).to_not be nil
+    s = Storage.create(name: 'Storage C', available_space: 70, price: 10, campus_dist: 0.9, rating: 2.3, start_date: '10-May-2024', end_date: '10-Aug-2024')
+    expect(Storage.find_by(name:'Storage C')).to_not be nil
 
     delete :destroy, id: s.id
-    expect(Storage.find_by(name:'Gargis home')).to be nil
+    expect(Storage.find_by(name:'Storage C')).to be nil
   end
 end
 
