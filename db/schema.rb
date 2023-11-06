68e93747f0101e47eb3c5a1a393e5f7b3b1a0ed1
Madhulika Balakumar <mb5144@columbia.edu>
2023-11-06 08:51:17 +0000

    Login, Register, Logout features

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20231029205858) do
+ActiveRecord::Schema.define(version: 20231106083241) do
 
   create_table "storages", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 20231029205858) do
     t.float    "rating"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "encrypted_password"
+    t.datetime "remember_created_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
   end
 
 end
