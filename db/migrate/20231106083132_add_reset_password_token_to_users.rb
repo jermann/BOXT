d512a650936eccd46b8e129e5ff4135ecf5809db
Tarang Donga <tkd2113@columbia.com>
2023-11-14 03:05:11 +0000

    Bug Fixes

--- db/migrate/20231106083132_add_reset_password_token_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddResetPasswordTokenToUsers < ActiveRecord::Migration[5.2]
-  def change
-    #add_column :users, :reset_password_token, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_13_042522) do
+ActiveRecord::Schema.define(version: 2023_11_14_022716) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,10 +43,9 @@ ActiveRecord::Schema.define(version: 2023_11_13_042522) do
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_storages_on_user_id"
   end
-
-  add_index "storages", ["user_id"], name: "index_storages_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
