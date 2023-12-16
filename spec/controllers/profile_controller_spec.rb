aecd08ec2076f23b3439b169fc6a02aa1f830194
Tarang Donga <tkd2113@columbia.com>
2023-12-16 07:10:26 +0000

    Rspec Testing

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -7,6 +7,8 @@ include Devise::Test::ControllerHelpers
 before(:each) do
   u1 = User.create!(name: 'Harry Potter', email: 'harry.potterp@example.com', password: 'wizard123')
   @u1 = u1
+  @s1 = Storage.create!(name: 'Storage C', available_space: 70, price: 10, campus_dist: 0.9, rating: 2.3, start_date: '10-May-2024', end_date: '10-Aug-2024', user: @u1)
+  @b1 = Booking.create!(user_id: @u1.id, storage_id: @s1.id)
   sign_in @u1
 end
 
@@ -19,9 +21,8 @@ end
 
 describe 'update' do
   it 'should update user ratings' do
-    booking = Booking.create!(user_id: @u1.id, storage_id: 1)
-    patch :update, params: {user_id: @u1.id, id: booking.storage_id, booking: {user_rating: 4.5}}
-    expect(booking.reload.user_rating).to eq(4.5)
+    patch :update, params: {user_id: @u1.id, id: @b1.storage_id, booking: {user_rating: 4.5}}
+    expect(@b1.reload.user_rating).to eq(4.5)
     expect(response).to redirect_to(profile_path(@u1))
   end
 end
